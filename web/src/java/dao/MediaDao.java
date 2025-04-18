224ddf407d672b4d7e5159bb2b66289f86ae5d8c
imane elkourari <imanekourari@gmail.com>
2025-04-18 17:52:48 +0200

    Update MediaDao.java

--- web/src/java/dao/MediaDao.java
+++ web/src/java/dao/MediaDao.java
@@ -1,13 +1,68 @@
 package dao;
 
 import entities.Media;
+import entities.TypeMedia;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
 
 public class MediaDao extends AbstractDao<Media> {
 
-    /**
-     *
-     */
     public MediaDao() {
         super(Media.class);
+    }
+    
+    // Rechercher des médias par titre
+    public List<Media> findByTitre(String titre) {
+        Session session = null;
+        Transaction tx = null;
+        List<Media> medias = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            medias = session.createNamedQuery("findByTitre", Media.class)
+                            .setParameter("titre", "%" + titre + "%")
+                            .getResultList();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return medias;
+    }
+
+    // Rechercher des médias par type de média
+    public List<Media> findByTypeMedia(TypeMedia typeMedia) {
+        Session session = null;
+        Transaction tx = null;
+        List<Media> medias = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            medias = session.createNamedQuery("findByTypeMedia", Media.class)
+                            .setParameter("id", typeMedia.getId())
+                            .getResultList();
+            tx.commit();
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return medias;
+    }
+
+    public Iterable<Media> findByTypeMedia(TypeMedia findById) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
